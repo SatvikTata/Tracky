1ae4fd202ded8c08bb46cc3f0b1b3e8c774a5926
SatvikTata <iamsatvik77@gmail.com>
2026-06-12 03:07:44 +0530

    Created springboot project in backend

---
+++ backend/src/main/java/com/tracky/TrackyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tracky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackyBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/tracky/TrackyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tracky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
