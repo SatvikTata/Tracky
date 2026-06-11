549142f55b5cf611cd0113f3671bc980bda5a855
SatvikTata <iamsatvik77@gmail.com>
2026-06-12 03:15:44 +0530

    Added health endpoint

---
+++ backend/src/main/java/com/tracky/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.tracky.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "Tracky Backend Running";
+    }
+}
